a88be2ef95ff943ba738e50c12d80bb9aad6022e
mostafa.abdelshafy <mostafa.abdelshafy88@gmail.com>
2020-08-03 08:13:30 +0200

    add the index and router files

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const socketio = require("socket.io");
+const http = require("http");
+const router = require("./router");
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+const server = http.createServer(app);
+const io = socketio(server);
+app.use(router);
+
+io.on("connect", (socket) => {
+  console.log("got connected!!!");
+  socket.on("disconnect", () => {
+    console.log("disconnected!!!");
+  });
+});
+server.listen(PORT, () => {
+  console.log(`server is up and listening on ${PORT}`);
+});
